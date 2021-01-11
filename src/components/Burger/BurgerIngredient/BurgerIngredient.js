bc20105a67fe9d4adb791003555db0f3acc3f681
Samiatrix <samarthchauhan519@gmail.com>
2021-01-11 18:24:51 +0530

    Before Backdrop Component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 import classes from './BurgerIngredient.module.css'
 
 const burgerIngredient = (props) => {
@@ -33,7 +33,7 @@ const burgerIngredient = (props) => {
     }
     return ingredient;
 };
-burgerIngredient.PropTypes = {
-    type: PropTypes.string.isRequired
+burgerIngredient.propTypes = {
+    type: propTypes.string.isRequired
 }
 export default burgerIngredient;
